4e70c3584c654e25604d14cd0116f6c967130ed1
andriimanzar <manzar20011@gmail.com>
2023-04-12 10:15:00 +0300

    Initial commit

---
+++ src/main/java/com/manzar/cinemarestapp/CinemaRestApplication.java
@@ -0,0 +1,16 @@
+package com.manzar.cinemarestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts application.
+ */
+@SpringBootApplication
+public class CinemaRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CinemaRestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/manzar/cinemarestapp/CinemaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manzar.cinemarestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
